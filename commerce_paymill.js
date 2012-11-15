60ed663fc5bd9f7ffff2a7d1e93388033186168b
Gareth Oakley <gaz at the2ndproject dot com>
2012-11-15 22:00:18 +0000

    Bind/unbind click handler

--- commerce_paymill.js
+++ commerce_paymill.js
@@ -1,18 +1,30 @@
-jQuery(document).ready(function($) {
-  $('#edit-continue').bind('click', function() {
-    event.preventDefault();
-    getToken($);
+jQuery(document).ready(function() {
+  // First load - bind the submit button
+  jQuery('#edit-continue').bind('click', paymillSubmit);
+  // Ensure changes to selected payment method bind/unbind
+  jQuery('input[name="commerce_payment[payment_method]"]').change(function() {
+    var selected = jQuery('input[name="commerce_payment[payment_method]"]:checked').val();
+	if (selected == 'commerce_paymill|commerce_payment_commerce_paymill') {
+      jQuery('#edit-continue').bind('click', paymillSubmit);
+	} else {
+      jQuery('#edit-continue').unbind('click', paymillSubmit);
+	}
   });
 });
 
-function getToken($) {
-  var cardHolderName = $('#payment-details input:eq(0)').val();
-  var number1 = $('#payment-details input:eq(1)').val();
-  var expMonth = $('#payment-details select:eq(0)').val();
-  var expYear = $('#payment-details select:eq(1)').val();
-  var cvc1 = $('#payment-details input:eq(2)').val();
-  var amount1 = $('input[name="commerce_payment[payment_details][credit_card][amount]"]').val();
-  var currency1 = $('input[name="commerce_payment[payment_details][credit_card][currency]"]').val();
+function paymillSubmit() {
+  event.preventDefault();
+  paymillGetToken();
+}
+
+function paymillGetToken() {
+  var cardHolderName = jQuery('#payment-details input:eq(0)').val();
+  var number1 = jQuery('#payment-details input:eq(1)').val();
+  var expMonth = jQuery('#payment-details select:eq(0)').val();
+  var expYear = jQuery('#payment-details select:eq(1)').val();
+  var cvc1 = jQuery('#payment-details input:eq(2)').val();
+  var amount1 = jQuery('input[name="commerce_payment[payment_details][credit_card][amount]"]').val();
+  var currency1 = jQuery('input[name="commerce_payment[payment_details][credit_card][currency]"]').val();
   paymill.config('3ds_cancel_label', PAYMILL_3DS_BUTTON_LABEL);
   paymill.createToken({
     number: number1,
@@ -28,7 +40,7 @@ function getToken($) {
 
 function paymillResponseHandler(error,result) {
   if (error) {
-    alert(error.apierror);
+    alert('Error: error.apierror');
   } else {
     jQuery('input[name="commerce_payment[payment_details][credit_card][token]"]').val(result.token);
     jQuery('#commerce-checkout-form-review').get(0).submit();
