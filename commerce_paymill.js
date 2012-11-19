a790a8bd10cc3c086d33847350bc68bc33635d13
Gareth Oakley <gaz at the2ndproject dot com>
2012-11-19 23:34:21 +0000

    Update to use Drupal JS libraries

--- commerce_paymill.js
+++ commerce_paymill.js
@@ -1,48 +1,102 @@
-jQuery(document).ready(function() {
-  // First load - bind the submit button
-  jQuery('#edit-continue').bind('click', paymillSubmit);
-  // Ensure changes to selected payment method bind/unbind
-  jQuery('input[name="commerce_payment[payment_method]"]').change(function() {
-    var selected = jQuery('input[name="commerce_payment[payment_method]"]:checked').val();
-    if (selected == 'commerce_paymill|commerce_payment_commerce_paymill') {
-      jQuery('#edit-continue').bind('click', paymillSubmit);
-    } else {
-      jQuery('#edit-continue').unbind('click', paymillSubmit);
+(function($) {
+
+  Drupal.behaviours.commercePaymill = {
+    attach: function(context, settings) {
+      // Configure Paymill bridge
+	  window.PAYMILL_PUBLIC_KEY = settings.publicKey;
+      paymill.config('3ds_cancel_label', Drupal.t('Cancel'));
+      // First load - bind the submit button
+      $('#edit-continue').bind('click', paymillSubmit);
+      // Ensure changes to selected payment method bind/unbind
+      $('input[name="commerce_payment[payment_method]"]').change(function() {
+        var selected = $('input[name="commerce_payment[payment_method]"]:checked').val();
+        if (selected == 'commerce_paymill|commerce_payment_commerce_paymill') {
+          $('#edit-continue').bind('click', paymillSubmit);
+        } else {
+          $('#edit-continue').unbind('click', paymillSubmit);
+        }
+      });
     }
-  });
-});
+  };
 
-function paymillSubmit() {
-  event.preventDefault();
-  paymillGetToken();
-}
-
-function paymillGetToken() {
-  var cardHolderName = jQuery('#payment-details input:eq(0)').val();
-  var number1 = jQuery('#payment-details input:eq(1)').val();
-  var expMonth = jQuery('#payment-details select:eq(0)').val();
-  var expYear = jQuery('#payment-details select:eq(1)').val();
-  var cvc1 = jQuery('#payment-details input:eq(2)').val();
-  var amount1 = jQuery('input[name="commerce_payment[payment_details][credit_card][amount]"]').val();
-  var currency1 = jQuery('input[name="commerce_payment[payment_details][credit_card][currency]"]').val();
-  paymill.config('3ds_cancel_label', PAYMILL_3DS_BUTTON_LABEL);
-  paymill.createToken({
-    number: number1,
-    exp_month: expMonth,
-    exp_year: expYear,
-    cvc: cvc1,
-    cardholdername: cardHolderName,
-    amount: amount1,
-    currency: currency1,
-  },
-  paymillResponseHandler);
-}
-
-function paymillResponseHandler(error,result) {
-  if (error) {
-    alert('Error: error.apierror');
-  } else {
-    jQuery('input[name="commerce_payment[payment_details][credit_card][token]"]').val(result.token);
-    jQuery('#commerce-checkout-form-review').get(0).submit();
+  function paymillSubmit() {
+    event.preventDefault();
+    paymillGetToken();
   }
-}
+
+  function paymillGetToken() {
+    var cardHolderName = $('#payment-details input:eq(0)').val();
+    var number1 = $('#payment-details input:eq(1)').val();
+    var expMonth = $('#payment-details select:eq(0)').val();
+    var expYear = $('#payment-details select:eq(1)').val();
+    var cvc1 = $('#payment-details input:eq(2)').val();
+    var amount1 = $('input[name="commerce_payment[payment_details][credit_card][amount]"]').val();
+    var currency1 = $('input[name="commerce_payment[payment_details][credit_card][currency]"]').val();
+      paymill.createToken({
+      number: number1,
+      exp_month: expMonth,
+      exp_year: expYear,
+      cvc: cvc1,
+      cardholdername: cardHolderName,
+      amount: amount1,
+      currency: currency1,
+    },
+    paymillResponseHandler);
+  }
+
+  function paymillResponseHandler(error,result) {
+    if (error) {
+      alert('Error: error.apierror');
+    } else {
+      $('input[name="commerce_payment[payment_details][credit_card][token]"]').val(result.token);
+      $('#commerce-checkout-form-review').get(0).submit();
+    }
+  }
+  
+  function paymillGetError(error) {
+    switch (error) {
+	  case 'internal_server_error':
+	    return Drupal.t('Communication with PSP failed');
+		break;
+	  case 'invalid_public_key':
+	    return Drupal.t('Invalid Public Key');
+		break;
+	  case 'unknown_error':
+	    return Drupal.t('Unknown Error');
+		break;
+	  case '3ds_cancelled':
+	    return Drupal.t('Password Entry of 3-D Secure password was cancelled by the user');
+		break;
+	  case 'field_invalid_card_number':
+	    return Drupal.t('Missing or invalid creditcard number');
+		break;
+	  case 'field_invalid_card_exp_year':
+	    return Drupal.t('Missing or invalid Expiry Year');
+		break;
+	  case 'field_invalid_card_exp_month':
+	    return Drupal.t('Missing or invalid Expiry Month');
+		break;
+	  case 'field_invalid_card_exp':
+	    return Drupal.t('Card is no longer valid or not anymore');
+		break;
+	  case 'field_invalid_card_cvc':
+	    return Drupal.t('Invalid Checking Number');
+		break;
+	  case 'field_invalid_card_holder':
+	    return Drupal.t('Invalid Cardholder');
+		break;
+	  case 'field_invalid_account_number':
+	    return Drupal.t('Missing or invalid bank account number');
+		break;
+	  case 'field_invalid_account_holder':
+	    return Drupal.t('Missing or invalid bank account holder');
+		break;
+	  case 'field_invalid_bank_code':
+	    return Drupal.t('Missing or invalid zip code');
+		break;
+	  default:
+		return Drupal.checkPlain(error.replace('_', ' '));
+	}
+  }
+
+})(jQuery);
